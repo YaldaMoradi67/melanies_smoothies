5d087a6af50c0f10fc1e840cdc38aa8f82ef519b
YaldaMoradi67 <myalda9679@gmail.com>
2026-04-01 08:08:19 +0200

    Import requests and fetch smoothie fruit data
    Added requests import and API call for smoothie fruit data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f"My Parents New Healthy Dinner")
@@ -22,6 +23,10 @@ ingredients_list = st.multiselect(
     , my_dataframe
     , max_selections = 5
 )
+
+
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
 
 if ingredients_list: 
 
